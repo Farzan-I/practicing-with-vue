e834ed48c3a71e1af36cd114cc1d0f1c093adf50
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-01-20 13:45:28 +0000

    Add first props to the web app

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div>
+    <input type="checkbox" id="todo-item"/>
+    <label for="to-do-item">{{label}}</label>
+  </div>
+
+</template>
+
+  <script>
+    export default {
+      props: {
+        label: {required: true, type: String},
+        done: {default: false, type: Boolean},
+      },
+    };
+  </script>
