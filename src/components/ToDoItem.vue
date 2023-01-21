d4823782aff06f339e0689860ad0cbbd251645d3
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-01-21 13:49:24 +0000

    Add data property to ToDoItem component

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -13,5 +13,10 @@
         label: {required: true, type: String},
         done: {default: false, type: Boolean},
       },
+      data() {
+        return {
+          isDone: this.done,
+        };
+      },
     };
   </script>
